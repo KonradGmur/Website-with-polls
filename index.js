ae549913215ff95c7576a8f5d19d82a098f179e6
Konrad <konrad.gmur@gmail.com>
2020-11-21 14:18:08 +0100

    initial project

---
+++ index.js
@@ -0,0 +1,35 @@
+// Slider
+new Splide(".splide", {
+  type: "loop",
+  perPage: 3,
+  breakpoints: {
+    1200: {
+      perPage: 3,
+    },
+    992: {
+      perPage: 3,
+    },
+    768: {
+      perPage: 2,
+    },
+    640: {
+      perPage: 1,
+    },
+  },
+}).mount();
+
+//Scroll
+
+// $(function () {
+//   $(window).scroll(function () {
+//     if ($(window).scrollTop() > 100) {
+//       $(".scrollTopButton").addClass("show");
+//     } else {
+//       $(".scrollTopButton").removeClass("show");
+//     }
+//   });
+
+//   $(".scrollTopButton").click(function () {
+//     $("body").animate({ scrollTop: 0 }, 400, "linear");
+//   });
+// });
